369eebe1e221c39897f99c1a425e48d8b0444b64
dindacellesta-dev <dindacellesta@gmail.com>
2025-11-23 11:01:57 +0700

    login

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-import ChatBox from "./components/ChatBox";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="p-6 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">AYO NGOBROL SAMA KUYO</h1>
-      <ChatBox />
-    </main>
-  );
+  redirect("/login"); 
 }
